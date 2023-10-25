d3c335646d6958e9fde668d3eb055f0157f0bc37
mikaeel khalid <mikaeelkhalid@icloud.com>
2023-10-25 09:07:14 +0200

    add sample express app S3 asset Zip

--- stacks/eb-codepipeline-stack.ts
+++ stacks/eb-codepipeline-stack.ts
@@ -1,9 +1,15 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { Asset } from 'aws-cdk-lib/aws-s3-assets';
 import { Construct } from 'constructs';
 
 export class EbCodePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // construct an S3 asset Zip from directory up.
+    const webAppZipArchive = new Asset(this, 'expressjs-app-zip', {
+      path: `${__dirname}/../express-app`,
+    });
   }
 }
 
